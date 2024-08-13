66f0663717017c19d1cc3f764e922550117f9ac8
Suraj Gyaneshwar Waghmare <waghmaresuraj159@gmail.com>
2024-08-13 15:37:15 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <TonConnectUIProvider manifestUrl="https://<YOUR_APP_URL>/tonconnect-manifest.json">
+        
+ <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </TonConnectUIProvider>
 )
