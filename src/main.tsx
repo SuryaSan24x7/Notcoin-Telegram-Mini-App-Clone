cab82564cd16ceda486f7af8c73553a4d970ae4a
Suraj Gyaneshwar Waghmare <waghmaresuraj159@gmail.com>
2024-08-21 14:53:16 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
-//manifestUrl="https://memetap-suryasan24x7s-projects.vercel.app/tonconnect-manifest.json"
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <TonConnectUIProvider >
+  <TonConnectUIProvider manifestUrl="https://memetap-suryasan24x7s-projects.vercel.app/tonconnect-manifest.json"
+  actionsConfiguration={{
+    twaReturnUrl: 'https://t.me/noob_memelord_nbot/start'
+  }}>
         
  <React.StrictMode>
     <App />
